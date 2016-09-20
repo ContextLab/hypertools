b1c18ee032109286244a5bb787f0788af9045663
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2016-09-20 15:09:43 -0400

    working on plot_coords (WIP)

--- python/plot_coords.py
+++ python/plot_coords.py
@@ -15,54 +15,61 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.misc import lena
 from sklearn.decomposition import PCA
 
 def plot_coords(x):
-	"""
-		inputs: TxD matrix of observations
-	           T-number of coords 
-	           D-dimensionality of each observation
-	           *Nans treated as missing observations
-	    outputs: 1-, 2-, or 3-dimensional representation of the data
-	"""
-    
-	def main_helper(x):
-		#determine how many dimensions (number of columns)
-		if x.shape[-1]==1:
-			bar(x)
-		if x.shape[-1]==2:
-			scatter2D(x)
-		if x.shape[-1]==3:
-			scatter3d(x)
-		if x.shape[-1]>3:
-			lowD(x)
+    """
+    inputs: TxD matrix of observations
+               T-number of coords
+               D-dimensionality of each observation
+               *Nans treated as missing observations
+            type (specify the type of plot)
+               if 'scatter', make a scatterplot
+               if 'line', make a line plot
+    outputs: 1-, 2-, or 3-dimensional representation of the data
+    """
+    typedict = dict()
+    typedict['scatter'] = ('k.', 'Axes3D.scatter')
+    typedict['line'] = ('k-', 'Axes3D.Line')
 
-	def bar(data):
-		#if 1d, make a bar graph
-		pos=np.arange(len(data))
-		plt.xticks(pos+0.4, pos)
-		plt.bar(pos,data)
-		plt.show()
+    def dispatch(x, type='scatter'):
+        #determine how many dimensions (number of columns)
+        if x.shape[-1]==1:
+            plot1D(x, type)
+        elif x.shape[-1]==2:
+            plot2D(x, type)
+        elif x.shape[-1]==3:
+            plot3D(x, type)
+        elif x.shape[-1]>3:
+            plot3D(reduceD(x, 3), type)
 
-	def scatter2D(data):
-		#if 2d, make a scatter
-		plt.scatter(data[:,0], data[:,1])
-		plt.show()
+    def plot1D(data, type):
+        x=np.arange(len(data))
+        plot2D(np.hstack((np.transpose(x), data), type))
 
-	def scatter3d(data):
-		#if 3d, make a 3d scatter
-		fig = plt.figure()
-		ax = fig.add_subplot(111, projection='3d')
-		ax.scatter(data[:,0], data[:,1], data[:,2], depthshade=True)
-		plt.show()
+    def plot2D(data, type):
+        # type: (object) -> object
+        #if 2d, make a scatter
+        plt.plot(data[:,0], data[:,1], typedict[type][0])
 
-	def lowD(x):
-		#if more than 3d, reduce to 3 (PCA), then re-run
-		m = PCA(n_components=3, whiten=True)
-		#n_components=3--> reduce to 3 dimensions
-		m.fit(x)
-		z = m.transform(x)
-		plot_coords(z)
+    def plot3D(data, type):
+        #if 3d, make a 3d scatter
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        c = [0, 0, 0]
+        plotfun = eval('ax.' + typedict[type][1])
+        plotfun(data[:,0], data[:,1], data[:,2], c=c, depthshade=True)
+
+    def reduceD(x, ndim):
+        #if more than 3d, reduce to 3 (PCA), then re-run
+        m = PCA(n_components=ndim, whiten=True)
+        #n_components=3--> reduce to 3 dimensions
+        m.fit(x)
+        return m.transform(x)
 		
-	main_helper(x)
+    dispatch(x)
+    plt.show()
+
+
+x=np.array([[1, 11, 21, 31], [2, 12, 22, 32], [3, 13, 33, 43], [4, 14, 44, 54]])
+plot_coords(x)
