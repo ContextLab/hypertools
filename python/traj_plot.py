a714e0bb2e14e711be34769e898ef70c1d1f6080
Kirsten <kirstenkmbziman@gmail.com>
2016-10-20 15:31:30 -0400

    PCA is separate; traj_plot.py demands matching columns, reduces list or array to 3D

--- python/traj_plot.py
+++ python/traj_plot.py
@@ -6,6 +6,7 @@
 
 ##PACKAGES##
 import numpy as np
+import PCA as PCA
 
 ##META##
 __authors__ = ["Jeremy Manning", "Kirsten Ziman"]
@@ -16,14 +17,39 @@ __emails__ = ["Jeremy.R.Manning@dartmouth.edu", "kirstenkmbziman@gmail.com", "co
 #__credits__ = [""]
 #__license__ = ""
 
-#FIRST STEPS
+#NEXT STEPS
 #++++++++++++++++
-#-PCA (3D)
-#	-essentially same base as plot_coords, but different animation.. 
 #-smooth to 100 samples per window (p chip to scale up by ten)
-#
+#send to plot_coords in "chunks"
+#use lazy evaluation to stream
 
-#THOUGHTS
-#+++++++++++++++++
-#why do PCA and plotting again when plot_coords already does it?
-#better to rewrite this as standalone script or to let it call on other scripts in the package?
+##MAIN FUNCTION##
+def traj(x):
+
+	##SUB FUNCTIONS##
+	def is_list(x):
+		if type(x[0][0])==np.ndarray:
+			return True
+		elif type(x[0][0])==np.int64:
+			return False
+
+	def col_match(j):
+		sizes_1=np.zeros(len(j))
+		for x in range(0, len(j)):
+			sizes_1[x]=j[x].shape[1]
+
+		if len(np.unique(sizes_1)) == 1:
+			return True
+		else:
+			return False
+
+##MAIN FUNCTION##
+if is_list(x) and x[0].shape[-1]>3 or not is_list(x) and x.shape[-1]>3:
+	data=PCA.reduce(x, 3)
+
+if not col_match(x):
+	print "Inputted arrays must have the same number of columns"
+
+else: data=x
+
+
