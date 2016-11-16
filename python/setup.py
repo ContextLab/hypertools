0c7ee27566d21ea1b0fe559f7bdc626e8d4c857c
andrewheusser <andrew.heusser@gmail.com>
2016-11-16 15:27:41 -0500

    added scikit-learn to requirements :

--- python/setup.py
+++ python/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='hyper-tools',
       version='1.1',
       py_modules=['plot_coords', 'hyperalign'],
-      install_requires=['numpy', 'scipy', 'matplotlib', 'seaborn']
+      install_requires=['numpy', 'scipy', 'matplotlib', 'seaborn', 'scikit-learn']
       )
