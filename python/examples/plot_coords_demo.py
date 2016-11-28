a9e2225fdcd6cbf0488de16a0f37a8c73eebf221
andrewheusser <andrew.heusser@gmail.com>
2016-11-28 11:15:42 -0500

    WIP implementing labels feature

--- python/examples/plot_coords_demo.py
+++ python/examples/plot_coords_demo.py
@@ -1,6 +1,17 @@
 import plot_coords as coords
 import scipy.io as sio
 
-data=sio.loadmat('../weights.mat')
+data=sio.loadmat('examples/weights.mat')
 w=data['weights'][0][:3]
-coords.plot_coords(w)
+
+labels=[]
+for idx,i in enumerate(w):
+    tmp=[]
+    for iidx,ii in enumerate(i):
+        if iidx==0:
+            tmp.append(iidx)
+        else:
+            tmp.append(None)
+    labels.append(tmp)
+
+coords.plot_coords(w,'o',labels=labels,hover=True)
--- python/plot_coords.py
+++ python/plot_coords.py
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from sklearn.decomposition import PCA
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
 import seaborn as sns
 
 ##MAIN FUNCTION##
@@ -41,6 +42,23 @@ def plot_coords(x, *args, **kwargs):
 		del kwargs['palette']
 	else:
 		sns.set_palette(palette="hls", n_colors=len(x))
+
+	##PARSE PLOT_COORDS SPECIFIC ARGS##
+	if 'ndims' in kwargs:
+		ndims=kwargs['ndims']
+		del kwargs['ndims']
+
+	if 'labels' in kwargs:
+		labels=kwargs['labels']
+		del kwargs['labels']
+
+	if 'hover' in kwargs:
+		kwargs['picker']=True
+		del kwargs['hover']
+		hover=True
+	else:
+		hover=False
+
 
 	##PARSE ARGS##
 	args_list = []
@@ -72,11 +90,6 @@ def plot_coords(x, *args, **kwargs):
 				tmp[kwarg]=kwargs[kwarg]
 		kwargs_list.append(tmp)
 
-	##PARSE PLOT_COORDS SPECIFIC ARGS##
-	if 'ndims' in kwargs:
-		ndims=kwargs['ndims']
-		del kwargs['ndims']
-
 	##SUB FUNCTIONS##
 	def is_list(x):
 		if type(x[0][0])==np.ndarray:
@@ -93,6 +106,92 @@ def plot_coords(x, *args, **kwargs):
 			return True
 		else:
 			return False
+
+	def add_labels(ax,data,labels,dims):
+		# check if labels and data are the same dims, else throw error
+		# if data.shape!=labels.shape:
+		# 	raise ValueError('Labels must be same shape as data.')
+		if dims==3:
+			for idx,point in enumerate(data):
+				if labels[idx]!=None:
+					x2, y2, _ = proj3d.proj_transform(point[0],point[1],point[2], ax.get_proj())
+					label = plt.annotate(
+						labels[idx],
+						xy = (x2, y2), xytext = (-20, 20),
+						textcoords = 'offset points', ha = 'right', va = 'bottom',
+						bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+						arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+	# def visualize3DData (X):
+    # """Visualize data in 3d plot with popover next to mouse position.
+	#
+    # Args:
+    #     X (np.array) - array of points, of shape (numPoints, 3)
+    # Returns:
+    #     None
+    # """
+    # fig = plt.figure(figsize = (16,10))
+    # ax = fig.add_subplot(111, projection = '3d')
+    # ax.scatter(X[:, 0], X[:, 1], X[:, 2], depthshade = False, picker = True)
+
+	def distance(point, event):
+		"""Return distance between mouse position and given data point
+
+		Args:
+			point (np.array): np.array of shape (3,), with x,y,z in data coords
+			event (MouseEvent): mouse event (which contains mouse position in .x and .xdata)
+		Returns:
+			distance (np.float64): distance (in screen coords) between mouse pos and data point
+		"""
+		assert point.shape == (3,), "distance: point.shape is wrong: %s, must be (3,)" % point.shape
+
+		# Project 3d data space to 2d data space
+		x2, y2, _ = proj3d.proj_transform(point[0], point[1], point[2], plt.gca().get_proj())
+		# Convert 2d data space to 2d screen space
+		x3, y3 = ax.transData.transform((x2, y2))
+
+		return np.sqrt ((x3 - event.x)**2 + (y3 - event.y)**2)
+
+
+	def calcClosestDatapoint(X, event):
+		""""Calculate which data point is closest to the mouse position.
+
+		Args:
+			X (np.array) - array of points, of shape (numPoints, 3)
+			event (MouseEvent) - mouse event (containing mouse position)
+		Returns:
+			smallestIndex (int) - the index (into the array of points X) of the element closest to the mouse position
+		"""
+		distances = [distance (X[i, 0:3], event) for i in range(X.shape[0])]
+		return np.argmin(distances)
+
+
+	def annotatePlot(X, index):
+		"""Create popover label in 3d chart
+
+		Args:
+			X (np.array) - array of points, of shape (numPoints, 3)
+			index (int) - index (into points array X) of item which should be printed
+		Returns:
+			None
+		"""
+		# If we have previously displayed another label, remove it first
+		if hasattr(annotatePlot, 'label'):
+			annotatePlot.label.remove()
+		# Get data point from array of points X, at position index
+		x2, y2, _ = proj3d.proj_transform(X[index, 0], X[index, 1], X[index, 2], ax.get_proj())
+		annotatePlot.label = plt.annotate( "Value %d" % index,
+		xy = (x2, y2), xytext = (-20, 20), textcoords = 'offset points', ha = 'right', va = 'bottom',
+			bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+			arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+		fig.canvas.draw()
+
+
+	def onMouseMotion(event):
+		"""Event that is triggered when mouse is moved. Shows text annotation over data point closest to mouse."""
+		closestIndex = calcClosestDatapoint(X, event)
+		annotatePlot (X, closestIndex)
+
 
 	#def resize(k):
 	#	sizes_1=np.zeros(len(k))
@@ -129,9 +228,9 @@ def plot_coords(x, *args, **kwargs):
 		elif x[0].shape[-1]==2:
 			plot2D_list(x)
 		elif x[0].shape[-1]==3:
-			plot3D_list(x)
+			return plot3D_list(x)
 		elif x[0].shape[-1]>3:
-			plot3D_list(reduceD_list(x, 3))
+			return plot3D_list(reduceD_list(x, 3))
 
 	def plot1D(data):
 		x=np.arange(len(data)).reshape((len(data),1))
@@ -183,6 +282,7 @@ def plot_coords(x, *args, **kwargs):
 			iargs = args_list[i]
 			ikwargs = kwargs_list[i]
 			ax.plot(data[i][:,0], data[i][:,1], data[i][:,2], *iargs, **ikwargs)
+		return fig,ax,data
 
 	def reduceD(x, ndim):
 		#if more than 3d, reduce and re-run
@@ -201,13 +301,17 @@ def plot_coords(x, *args, **kwargs):
 
 	##MAIN FUNCTION##
 	if is_list(x):
-		#dispatch_list(resize(x))
 		if col_match(x):
-			dispatch_list(x)
+			fig,ax,X = dispatch_list(x)
+			if hover:
+				X = np.hstack(X)
+				fig.canvas.mpl_connect('motion_notify_event', onMouseMotion)  # on mouse motion
 			plt.show()
 		else:
 			print "Inputted arrays must have the same number of columns"
 
 	else:
 		dispatch(x)
+		if hover:
+			fig.canvas.mpl_connect('motion_notify_event', onMouseMotion)  # on mouse motion
 		plt.show()
