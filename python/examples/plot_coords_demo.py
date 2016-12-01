0dd1b2b03ef18ff62868d27972f4dce43ef2d57c
andrewheusser <andrew.heusser@gmail.com>
2016-12-01 11:18:05 -0500

    initial commit for point_colors

--- python/examples/plot_coords_demo.py
+++ python/examples/plot_coords_demo.py
@@ -5,14 +5,27 @@ import numpy as np
 data=sio.loadmat('examples/weights.mat')
 w=data['weights'][0][0:3]
 
-labels=[]
+# labels=[]
+# for idx,i in enumerate(w):
+#     tmp=[]
+#     for iidx,ii in enumerate(i):
+#         if iidx==0:
+#             tmp.append('Point ' + str(idx))
+#         else:
+#             tmp.append(None)
+#     labels.append(tmp)
+#
+# coords.plot_coords(w,'o',labels=labels)
+
+# example of point colors
+point_colors=[]
 for idx,i in enumerate(w):
     tmp=[]
     for iidx,ii in enumerate(i):
         if iidx==0:
-            tmp.append('Point ' + str(idx))
+            tmp.append('first_point')
         else:
-            tmp.append(None)
-    labels.append(tmp)
+            tmp.append('other_points')
+    point_colors.append(tmp)
 
-coords.plot_coords(w,'o',labels=labels)
+coords.plot_coords(w,'o',point_colors=point_colors)
