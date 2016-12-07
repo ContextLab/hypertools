cde65b259ba5080668f9e45fe02447b08dbbe677
Andy Heusser <andyheusser@andys-mbp.wireless.dartmouth.edu>
2016-12-07 12:04:26 -0500

    WIP scaling axes, camera position

--- python/animate.py
+++ python/animate.py
@@ -6,7 +6,7 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
 from scipy.interpolate import PchipInterpolator as pchip
-import PCA as PCA
+from sklearn.decomposition import PCA as PCA
 import hyperalign as hyp
 
 import seaborn as sns
@@ -35,29 +35,29 @@ def interp_array_list(arr_list,interp_val=10):
     return smoothed
 
 def get_cube_scale(x):
-    if x is list:
-        x = np.vstack(x)
-    x = np.square(x)
-    x = np.sum(x,1)
-    return np.max(x)
+    x = np.vstack(x)
+    x_square = np.square(x)
+    x_ss = np.sum(x_square,axis=1)
+    idx = [i for i in range(len(x_ss)) if x_ss[i]==np.max(x_ss)]
+    print(np.linalg.norm(x[idx,:]))
+    return np.linalg.norm(x[idx,:])
 
 def animate(x):
 
-    def update_lines(num, dataLines, lines, trailLines, tail_len=50, tail_style=':', speed=1, cam_dist=5):
+    def update_lines(num, data_lines, lines, trail_lines, cube_scale, tail_len=50, tail_style=':', speed=1):
 
         if hasattr(update_lines, 'planes'):
             for plane in update_lines.planes:
                 plane.remove()
 
-        update_lines.planes = plot_cube()
+        update_lines.planes = plot_cube(cube_scale)
         ax.view_init(elev=10, azim=speed*num/5)
-        # ax.dist=cam_dist
+        ax.dist=6
 
-        for line, data, trail in zip(lines, dataLines, trailLines):
+        for line, data, trail in zip(lines, data_lines, trail_lines):
             if num<=tail_len:
                     line.set_data(data[0:num+1, 0:2].T)
                     line.set_3d_properties(data[0:num+1, 2])
-
             else:
                 line.set_data(data[num-tail_len:num+1, 0:2].T)
                 line.set_3d_properties(data[num-tail_len:num+1, 2])
@@ -70,7 +70,7 @@ def animate(x):
                     trail.set_data(data[0:1+num-tail_len, 0:2].T)
                     trail.set_3d_properties(data[0:1+num-tail_len, 2])
                     trail.set_linestyle(tail_style)
-        return lines,trailLines
+        return lines,trail_lines
 
     # Attaching 3D axis to the figure
     fig = plt.figure()
@@ -81,17 +81,9 @@ def animate(x):
     lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1], linewidth=3)[0] for dat in x]
     trail = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in x]
 
-    # Setting the axes properties
-    ax.set_xlim3d([0, 1])
-    ax.set_ylim3d([0, 1])
-    ax.set_zlim3d([0, 1])
-
-    # ax.grid(False)
-
     ax.set_axis_off()
 
-
-    def plot_cube(scale=1):
+    def plot_cube(scale, const=1.25):
         cube = {
             "top"    : ( [[-1,1],[-1,1]], [[-1,-1],[1,1]], [[1,1],[1,1]] ),
             "bottom" : ( [[-1,1],[-1,1]], [[-1,-1],[1,1]], [[-1,-1],[-1,-1]] ),
@@ -104,17 +96,24 @@ def animate(x):
         plane_list = []
         for side in cube:
             (Xs, Ys, Zs) = (
-                np.asarray(cube[side][0])*scale,
-                np.asarray(cube[side][1])*scale,
-                np.asarray(cube[side][2])*scale
+                np.asarray(cube[side][0])*scale*const,
+                np.asarray(cube[side][1])*scale*const,
+                np.asarray(cube[side][2])*scale*const
                 )
             plane_list.append(ax.plot_wireframe(Xs, Ys, Zs, rstride=1, cstride=1, color='black', linewidth=3))
         return plane_list
 
+    # Get cube scale from data
+    cube_scale = get_cube_scale(x)
+
+    # Setting the axes properties
+    ax.set_xlim3d([-cube_scale, cube_scale])
+    ax.set_ylim3d([-cube_scale, cube_scale])
+    ax.set_zlim3d([-cube_scale, cube_scale])
 
     # Creating the Animation object
-    line_ani = animation.FuncAnimation(fig, update_lines, 1000, fargs=(x, lines, trail),
-                                   interval=8, blit=False, repeat=True)
+    line_ani = animation.FuncAnimation(fig, update_lines, 1000, fargs=(x, lines, trail, cube_scale),
+                                   interval=8, blit=False)
     plt.show()
 
 ################################################################################
@@ -139,13 +138,13 @@ def Gen_RandLine(length, dims=2):
 
     return lineData
 
-import scipy.io as sio
+# import scipy.io as sio
+#
+# data = sio.loadmat('extras/example_data/weights.mat')
+# test_data=data['weights'][0][1]
+# test_data2=data['weights'][0][2]
+# test_data3=data['weights'][0][3]
+# data = np.array([test_data, test_data2, test_data3])
 
-data = sio.loadmat('extras/example_data/weights.mat')
-test_data=data['weights'][0][1]
-test_data2=data['weights'][0][2]
-test_data3=data['weights'][0][3]
-data = np.array([test_data, test_data2, test_data3])
-
-# data = [Gen_RandLine(1000,3).T for index in range(3)]
+data = [Gen_RandLine(100,3).T for index in range(3)]
 animate(data)
