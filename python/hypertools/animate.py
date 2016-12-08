59def1cbdc0343e75aabec65a93b34838755c2fb
andrewheusser <andrew.heusser@gmail.com>
2016-12-08 12:42:46 -0500

    refactor continued

--- python/hypertools/animate.py
+++ python/hypertools/animate.py
@@ -5,53 +5,17 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
-from scipy.interpolate import PchipInterpolator as pchip
-from sklearn.decomposition import PCA as PCA
-
 from .helpers import *
 
 def animate(x, *args, **kwargs):
 
-    def update_lines(num, data_lines, lines, trail_lines, cube_scale, tail_len=50, tail_style=':', speed=1):
+    ##SUB FUNCTIONS##
+    def get_cube_scale(x, c):
+        x = np.vstack(x)
+        dists = np.sqrt(np.sum(np.square(x), axis=1))
+        return c*np.max(dists)
 
-        if hasattr(update_lines, 'planes'):
-            for plane in update_lines.planes:
-                plane.remove()
-
-        update_lines.planes = plot_cube(cube_scale)
-        ax.view_init(elev=10, azim=speed*num/5)
-        ax.dist=6
-
-        for line, data, trail in zip(lines, data_lines, trail_lines):
-            if num<=tail_len:
-                    line.set_data(data[0:num+1, 0:2].T)
-                    line.set_3d_properties(data[0:num+1, 2])
-            else:
-                line.set_data(data[num-tail_len:num+1, 0:2].T)
-                line.set_3d_properties(data[num-tail_len:num+1, 2])
-            if num>=tail_len:
-                if num>=tail_len*2:
-                    trail.set_data(data[num-tail_len*2:1+num-tail_len, 0:2].T)
-                    trail.set_3d_properties(data[num-tail_len*2:1+num-tail_len, 2])
-                    trail.set_linestyle(tail_style)
-                else:
-                    trail.set_data(data[0:1+num-tail_len, 0:2].T)
-                    trail.set_3d_properties(data[0:1+num-tail_len, 2])
-                    trail.set_linestyle(tail_style)
-        return lines,trail_lines
-
-    # Attaching 3D axis to the figure
-    fig = plt.figure()
-    ax = p3.Axes3D(fig)
-
-    x = interp_array_list(x)
-
-    lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1], linewidth=3)[0] for dat in x]
-    trail = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in x]
-
-    ax.set_axis_off()
-
-    def plot_cube(scale, const=1.25):
+    def plot_cube(scale, const=1):
         cube = {
             "top"    : ( [[-1,1],[-1,1]], [[-1,-1],[1,1]], [[1,1],[1,1]] ),
             "bottom" : ( [[-1,1],[-1,1]], [[-1,-1],[1,1]], [[-1,-1],[-1,-1]] ),
@@ -71,8 +35,49 @@ def animate(x, *args, **kwargs):
             plane_list.append(ax.plot_wireframe(Xs, Ys, Zs, rstride=1, cstride=1, color='black', linewidth=3))
         return plane_list
 
+    def update_lines(num, data_lines, lines, trail_lines, cube_scale, tail_len=30, tail_style=':', speed=1):
+
+        if hasattr(update_lines, 'planes'):
+            for plane in update_lines.planes:
+                plane.remove()
+
+        update_lines.planes = plot_cube(cube_scale)
+        ax.view_init(elev=10, azim=speed*num/5)
+        ax.dist=cube_scale-1
+
+        for line, data, trail in zip(lines, data_lines, trail_lines):
+            if num<=tail_len:
+                    line.set_data(data[0:num+1, 0:2].T)
+                    line.set_3d_properties(data[0:num+1, 2])
+            else:
+                line.set_data(data[num-tail_len:num+1, 0:2].T)
+                line.set_3d_properties(data[num-tail_len:num+1, 2])
+            # if num>=tail_len:
+            #     if num>=tail_len*2:
+            #         trail.set_data(data[num-tail_len*2:1+num-tail_len, 0:2].T)
+            #         trail.set_3d_properties(data[num-tail_len*2:1+num-tail_len, 2])
+            #         trail.set_linestyle(tail_style)
+            #     else:
+            #         trail.set_data(data[0:1+num-tail_len, 0:2].T)
+            #         trail.set_3d_properties(data[0:1+num-tail_len, 2])
+            #         trail.set_linestyle(tail_style)
+        return lines,trail_lines
+
+    # Attaching 3D axis to the figure
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    x = reduceD_list(x,3)
+    x = interp_array_list(x)
+    x = center(x)
+
+    lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1], linewidth=3)[0] for dat in x]
+    trail = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in x]
+
+    ax.set_axis_off()
+
     # Get cube scale from data
-    cube_scale = get_cube_scale(x)
+    cube_scale = get_cube_scale(x,1)
 
     # Setting the axes properties
     ax.set_xlim3d([-cube_scale, cube_scale])
--- python/hypertools/describe.py
+++ python/hypertools/describe.py
@@ -1,1 +1,34 @@
-a=1
+from __future__ import division
+import numpy as np
+from sklearn import PCA
+from scipy.spatial.distance import pdist
+
+import seaborn as sns
+sns.set(style="darkgrid")
+
+def describe(x):
+
+    ##SUB FUNCTIONS##
+
+    def describe_align(x):
+        if type(x) not list:
+            print('Must pass a list of arrays.')
+        
+
+    def describe_PCA(x):
+        if type(x) is list:
+            x = np.vstack(x)
+        cov_alldims = pdist(x,'correlation')
+        cov_PCA =  [(pdist(reduceD(x,num),'correlation')) for num in range(2,x.shape[1]+1)]
+        return [np.corrcoef(cov_alldims, cov_PCA_i)[0][1] for cov_PCA_i in cov_PCA]
+
+    if type(x) is list:
+        continue
+    else:
+        x = [x]
+
+    attrs = {}
+
+    attrs['PCA_summary'] = {}
+    attrs['PCA_summary']['average'] = describe_PCA(x)
+    attrs['PCA_summary']['individual'] = [describe_PCA(x_i) for x_i in x]
