f3374319fa11167abcdd4e986e3549804bc0caf8
andrewheusser <andrew.heusser@gmail.com>
2016-12-12 18:22:12 -0500

    update demos

--- python/examples/hypertools_demo-describe.py
+++ python/examples/hypertools_demo-describe.py
@@ -3,7 +3,6 @@ import scipy.io as sio
 import numpy as np
 
 data=sio.loadmat('./weights.mat')
-w=data['weights'][0][0:3]
-w = [i for i in w]
+w=[i for i in data['weights'][0][0:3]]
 
 hyp.describe(w)
--- python/examples/hypertools_demo-point_colors.py
+++ python/examples/hypertools_demo-point_colors.py
@@ -2,8 +2,8 @@ import hypertools as hyp
 import scipy.io as sio
 import numpy as np
 
-data=sio.loadmat('./weights.mat')
-w=data['weights'][0][0:3]
+data=sio.loadmat('weights.mat')
+w=[i for i in data['weights'][0][0:3]]
 
 point_colors=[]
 for idx,i in enumerate(w):
