a2fd3733751db229e45647adedc91f5d092a9b5e
andrewheusser <andrew.heusser@gmail.com>
2016-12-14 10:11:32 -0500

    working version of align

--- python/examples/hypertools_demo-animate.py
+++ python/examples/hypertools_demo-animate.py
@@ -3,8 +3,8 @@ import scipy.io as sio
 import numpy as np
 
 data=sio.loadmat('./weights.mat')
-
 w = data['weights'][0]
 w = [i for i in w]
-# aligned = hyp.align(w)
+
+aligned = hyp.align(w)
 hyp.plot(w,animate=True)
