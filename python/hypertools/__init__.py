e6e38a587180eddf24b26a1059af48bb8ae93502
andrewheusser <andrew.heusser@gmail.com>
2016-12-16 14:36:38 -0500

    implemented haxby alg with new procrustes implementation

--- python/hypertools/__init__.py
+++ python/hypertools/__init__.py
@@ -4,14 +4,14 @@ from .plot import plot
 from .align import align
 from .reduce import reduce
 from .describe import describe
-from .procrustean import Procrustes
+from .procrustean import procrustes
 
 class hypertools(object):
     '''Hypertools module'''
 
-    def __init__(self, plot=plot, align=align, reduce=reduce, describe=describe, procrustes=Procrustes):
+    def __init__(self, plot=plot, align=align, reduce=reduce, describe=describe, procrustes=procrustes):
         self.plot = plot.plot
         self.align = align.align
         self.reduce = reduce.reduce
         self.describe = describe.describe
-        self.Procrustes = Procrustes
+        self.procrustes = procrustes
--- python/hypertools/align.py
+++ python/hypertools/align.py
@@ -20,22 +20,57 @@ OUTPUTS:
 
 ##PACKAGES##
 from srm import SRM
-from procrustean import Procrustes
+from procrustean import procrustes
 import numpy as np
 
 ##MAIN FUNCTION##
-def align(data, method='HYP'):
+def align(data, method='HYPER'):
 	"""Implements hyperalignment"""
 
 	assert all(isinstance(i, np.ndarray) for i in data) and type(data) is list and len(data)>1, "Input must be list of arrays"
 
-	if method=='HYP':
-		for item in data:
-			pc = Procrustes()
-			pc._train(w1[0],w1[1].T)
+	if method=='HYPER':
 
+		##STEP 0: STANDARDIZE SIZE AND SHAPE##
+		sizes_0 = map(lambda x: x.shape[0], data)
+		sizes_1 = map(lambda x: x.shape[1], data)
 
+		#find the smallest number of rows
+		R = min(sizes_0)
+		C = max([3, max(sizes_1)])
 
+		m = [np.empty((R,C), dtype=np.ndarray)] * len(data)
+
+		for idx,x in enumerate(data):
+			y = x[0:R,:]
+			missing = C - y.shape[1]
+			add = np.zeros((y.shape[0], missing))
+			y = np.append(y, add, axis=1)
+			m[idx]=y
+
+		##STEP 1: TEMPLATE##
+		for x in range(0, len(m)):
+			if x==0:
+				template = m[x]
+			else:
+				next = procrustes(np.transpose(template / (x + 1)), np.transpose(m[x]))
+				template += np.transpose(next)
+		template /= len(m)
+
+		##STEP 2: NEW COMMON TEMPLATE##
+		#align each subj to the template from STEP 1
+		template2 = np.zeros(template.shape)
+		for x in range(0, len(m)):
+			next = procrustes(np.transpose(template), np.transpose(m[x]))
+			template2 += np.transpose(next)
+		template2 /= len(m)
+
+		#STEP 3 (below): ALIGN TO NEW TEMPLATE
+		aligned = [np.zeros(template2.shape)] * len(m)
+		for x in range(0, len(m)):
+			next = procrustes(np.transpose(template2), np.transpose(m[x]))
+			aligned[x] = np.transpose(next)
+		return aligned
 
 	elif method=='SRM':
 		data = [i.T for i in data]
