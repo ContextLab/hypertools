a9182a96eb37c5574001dd89cc232a7b2599bf3c
andrewheusser <andrew.heusser@gmail.com>
2016-12-16 14:00:19 -0500

    saving in case I screw it up during cleanup

--- python/hypertools/__init__.py
+++ python/hypertools/__init__.py
@@ -4,12 +4,14 @@ from .plot import plot
 from .align import align
 from .reduce import reduce
 from .describe import describe
+from .procrustean import Procrustes
 
 class hypertools(object):
     '''Hypertools module'''
 
-    def __init__(self, plot=plot, align=align, reduce=reduce, describe=describe):
+    def __init__(self, plot=plot, align=align, reduce=reduce, describe=describe, procrustes=Procrustes):
         self.plot = plot.plot
         self.align = align.align
         self.reduce = reduce.reduce
         self.describe = describe.describe
+        self.Procrustes = Procrustes
