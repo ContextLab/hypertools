2a48e8d32708a778d205b3f3712961fb08b0db5c
andrewheusser <andrew.heusser@gmail.com>
2016-12-19 09:46:34 -0500

    removed interpolation

--- python/hypertools/reduce.py
+++ python/hypertools/reduce.py
@@ -19,14 +19,14 @@ from sklearn.decomposition import PCA as PCA
 from .helpers import *
 
 def reducePCA(x, ndim):
+
 	if np.isnan(np.vstack(x)).any():
 		warnings.warn('Missing data: Inexact solution computed with PPCA (see https://github.com/allentran/pca-magic for details)')
 		x_split= np.cumsum([i.shape[0] for i in x][:-1])
 		m = PPCA(np.vstack(x))
 		m.fit(d=ndim)
-		x_pca = m.transform(np.vstack(x))
-		x_pca_interp = interp_col_nans(x_pca)
-		return list(np.split(x_pca_interp,x_split,axis=0))
+		x_pca = m.transform()
+		return list(np.split(x_pca,x_split,axis=0))
 	else:
 		m=PCA(n_components=ndim, whiten=True)
 		m.fit(np.vstack(x))
@@ -34,5 +34,7 @@ def reducePCA(x, ndim):
 
 ##MAIN FUNCTION##
 def reduce(arr,ndims=3, method='PCA'):
-    if method=='PCA':
-        return reducePCA(arr,ndims)
+	if type(arr) is not list:
+		arr = [arr]
+	if method=='PCA':
+		return reducePCA(arr,ndims)
