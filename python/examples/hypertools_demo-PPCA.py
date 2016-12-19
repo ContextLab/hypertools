ab58ffb31a31fe93823c2792fa2c0cc779a0ec40
andrewheusser <andrew.heusser@gmail.com>
2016-12-19 09:05:27 -0500

    PPCA updated

--- python/examples/hypertools_demo-PPCA.py
+++ python/examples/hypertools_demo-PPCA.py
@@ -7,9 +7,11 @@ K = 10 - toeplitz(np.arange(10))
 
 data1 = np.cumsum(np.random.multivariate_normal(np.zeros(10), K, 250), axis=0)
 data2 = copy(data1)
-sample_inds = np.random.choice(data2.shape[0], data2.shape[0]*.1)
-for sample in sample_inds:
-    data2[sample,:]=np.nan
 
-print(data2[sample,0])
-hyp.plot([data1,data2])
+missing = .1
+inds = [(i,j) for i in range(data2.shape[0]) for j in range(data2.shape[1])]
+missing_data = [inds[i] for i in np.random.choice(len(inds), len(inds)*missing)]
+for i,j in missing_data:
+    data2[i,j]=np.nan
+
+hyp.plot([data1,data2],linestyle=['-',':'])
