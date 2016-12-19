fc66300c4f3c9e0f0cccbd34e65118ffb7272dd8
andrewheusser <andrew.heusser@gmail.com>
2016-12-19 15:46:17 -0500

    missing data example

--- python/hypertools/util/missing_inds.py
+++ python/hypertools/util/missing_inds.py
@@ -6,7 +6,7 @@ import numpy as np
 def missing_inds(x):
 	if type(x) is not list:
 		x = [x]
-	inds = [[(i,j) for i in range(arr.shape[0]) for j in range(arr.shape[1]) if np.isnan(arr[i,j])] for arr in x]
+	inds = [[idx for idx,row in enumerate(arr) if any(np.isnan(row))] for arr in x]
 	if len(inds)>1:
 		return inds
 	else:
