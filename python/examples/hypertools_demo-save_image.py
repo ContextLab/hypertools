687f62c5fa7e00cd6ca9b4ab4c5cf0ded3a21232
andrewheusser <andrew.heusser@gmail.com>
2016-12-24 17:51:37 -0500

    updated demos, moved sample data

--- python/examples/hypertools_demo-save_image.py
+++ python/examples/hypertools_demo-save_image.py
@@ -2,7 +2,7 @@ import hypertools as hyp
 import scipy.io as sio
 import numpy as np
 
-data=sio.loadmat('weights.mat')
+data=sio.loadmat('sample_data/weights.mat')
 w=[i for i in data['weights'][0][0:3]]
 
 hyp.plot(w,'o', save_path='test-image.pdf')
