8c058736c5716771b17c095b20f61f2312da0a08
andrewheusser <andrew.heusser@gmail.com>
2016-12-24 16:03:27 -0500

    pandas df support

--- python/examples/hypertools_demo-pandas.py
+++ python/examples/hypertools_demo-pandas.py
@@ -3,11 +3,4 @@ import pandas as pd
 
 data=pd.read_csv('sample_data/mushrooms.csv')
 
-df = pd.DataFrame()
-df['class'] = data['class']
-df['class'] = df['class'].replace('p', 1).replace('e', 0)
-for colname in data.columns[1:]:
-    df = df.join(pd.get_dummies(data[colname], prefix=colname))
-
-data = df[df.columns[1:]]
-hyp.plot(data,'o')
+hyp.plot(data,'o', point_colors=data['class'])
