3b43b25f807abc7d3bd3fa32e2ca43397ade2412
andrewheusser <andrew.heusser@gmail.com>
2016-12-29 17:37:15 -0500

    removed a few more functions that arent being used

--- hypertools/_shared/helpers.py
+++ hypertools/_shared/helpers.py
@@ -78,44 +78,6 @@ def check_data(data):
 	else:
 		raise ValueError("Data must be numpy array, list of numpy array or pandas dataframe.")
 
-def _getAplus(A):
-	eigval, eigvec = np.linalg.eig(A)
-	Q = np.matrix(eigvec)
-	xdiag = np.matrix(np.diag(np.maximum(eigval, 0)))
-	return Q*xdiag*Q.T
-
-def _getPs(A, W=None):
-	W05 = np.matrix(W**.5)
-	return  W05.I * _getAplus(W05 * A * W05) * W05.I
-
-def _getPu(A, W=None):
-	Aret = np.array(A.copy())
-	Aret[W > 0] = np.array(W)[W > 0]
-	return np.matrix(Aret)
-
-def nearPD(A, nit=10):
-	n = A.shape[0]
-	W = np.identity(n)
-	# W is the matrix used for the norm (assumed to be Identity matrix here)
-	# the algorithm should work for any diagonal W
-	deltaS = 0
-	Yk = A.copy()
-	for k in range(nit):
-		Rk = Yk - deltaS
-		Xk = _getPs(Rk, W=W)
-		deltaS = Xk - Rk
-		Yk = _getPu(Xk, W=W)
-	return Yk
-
-def is_pos_def(x):
-	return np.all(np.linalg.eigvals(x)>0)
-
-def make_pos_def(x):
-	if is_pos_def(x):
-		return x
-	else:
-		return nearPD(x)
-
 def nan_helper(y):
 	"""Helper to handle indices and logical indices of NaNs.
 	"""
