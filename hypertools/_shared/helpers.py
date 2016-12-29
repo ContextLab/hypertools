fff6b649cdd71c08c83adbb71bff131cd1a8fd16
andrewheusser <andrew.heusser@gmail.com>
2016-12-29 16:02:37 -0500

    working on helpers tests

--- hypertools/_shared/helpers.py
+++ hypertools/_shared/helpers.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 ##HELPER FUNCTIONS##
 def center(x):
-	assert type(x) is list, "Input to center must be list"
+	assert type(x) is list, "Input data to center must be list"
 	x_stacked = np.vstack(x)
 	return [i - np.mean(x_stacked, 0) for i in x]
 
@@ -43,27 +43,24 @@ def vals2colors(vals,cmap='GnBu_d',res=100):
 	return [tuple(i) for i in palette[ranks, :]]
 
 def vals2bins(vals,res=100):
-	"""Maps values to colors
+	"""Maps values to bins
 	Args:
 	values (list or list of lists) - list of values to map to colors
-	cmap (str) - color map (default is 'husl')
 	res (int) - resolution of the color map (default: 100)
 	Returns:
-	list of rgb tuples
+	list of numbers representing bins
 	"""
 	# flatten if list of lists
 	if any(isinstance(el, list) for el in vals):
 		vals = list(itertools.chain(*vals))
-	return np.digitize(vals, np.linspace(np.min(vals), np.max(vals)+1, res+1)) - 1
+	return list(np.digitize(vals, np.linspace(np.min(vals), np.max(vals)+1, res+1)) - 1)
 
-# #  this will be moved to utils.py
 def interp_array(arr,interp_val=10):
     x=np.arange(0, len(arr), 1)
     xx=np.arange(0, len(arr)-1, 1/interp_val)
     q=pchip(x,arr)
     return q(xx)
 
-# #  this will be moved to utils.py
 def interp_array_list(arr_list,interp_val=10):
     smoothed= [np.zeros(arr_list[0].shape) for item in arr_list]
     for idx,arr in enumerate(arr_list):
