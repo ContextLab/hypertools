13c2cf7ab5cec1586e8121c26672c984918907e1
andrewheusser <andrew.heusser@gmail.com>
2016-12-30 19:45:43 -0500

    improved explore mode code

---
+++ examples/hypertools_demo-explore.py
@@ -0,0 +1,8 @@
+import hypertools as hyp
+import scipy.io as sio
+import numpy as np
+
+data=sio.loadmat('sample_data/weights.mat')
+w=[i for i in data['weights'][0][0:2]]
+
+hyp.plot(w,'o', explore=True)
