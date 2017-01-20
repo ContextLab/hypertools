2165d1e6cea30ff82aef7d53d0ffe66ddc7aa6b3
andrewheusser <andrew.heusser@gmail.com>
2017-01-20 10:44:46 -0500

    updated install_requires and added minimum versions needed

--- setup.py
+++ setup.py
@@ -20,5 +20,14 @@ setup(
     author_email='contextualdynamics@gmail.com',
     url='https://github.com/ContextLab/hypertools',
     license=license,
-    packages=find_packages(exclude=('tests', 'examples', 'images'))
+    packages=find_packages(exclude=('tests', 'examples', 'images')),
+    install_requires=[
+   'PPCA>=0.0.2',
+   'scikit-learn>=0.18.1',
+   'pandas>=0.18.0',
+   'seaborn>=0.7.1',
+   'matplotlib>=1.5.1',
+   'scipy>=0.17.1',
+   'numpy>=1.10.4'
+   ]
 )
