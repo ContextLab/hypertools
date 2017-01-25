a20b82804a5cbbd2cb8d1976e832a3237926b93f
andrewheusser <andrew.heusser@gmail.com>
2017-01-25 16:53:55 -0500

    fixed bug w save_movie example

--- examples/save_movie.py
+++ examples/save_movie.py
@@ -5,6 +5,11 @@ import os
 
 datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)),'sample_data/')
 data=sio.loadmat(datadir + 'weights.mat')
-w = [i for i in data['weights'][0]]
-aligned = hyp.align(w)
-hyp.plot(aligned, animate=True, movie_len=10, save_path='test-movie.mp4')
+w=data['weights'][0]
+w = [i for i in w]
+aligned_w = hyp.tools.align(w)
+
+w1 = np.mean(aligned_w[:17],0)
+w2 = np.mean(aligned_w[18:],0)
+
+hyp.plot([w1,w2], animate=True, duration=120, save_path='animation.mp4')
