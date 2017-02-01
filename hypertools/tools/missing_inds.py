2d5aa6ff901b0650bf0b92a87d64fb0d110c7523
andrewheusser <andrew.heusser@gmail.com>
2017-02-01 15:17:57 -0500

    added docstring to missing_inds.py

--- hypertools/tools/missing_inds.py
+++ hypertools/tools/missing_inds.py
@@ -5,11 +5,35 @@ import numpy as np
 from .._shared.helpers import format_data
 
 def missing_inds(x):
+    """
+    Returns indices of missing data (i.e. nans)
 
-	x = format_data(x)
+    This function is useful to identify rows of your array that contain missing
+    data or nans.  The returned indices can be used to remove the rows with
+    missing data, or label the missing data points that are interpolated
+    using PPCA.
 
-	inds = [[idx for idx,row in enumerate(arr) if any(np.isnan(row))] for arr in x]
-	if len(inds)>1:
-		return inds
-	else:
-		return inds[0]
+    Parameters
+    ----------
+    x : array or list of arrays
+
+
+    Returns
+    ----------
+    x : Numpy array or list of arrays
+        This can be either a single array, or a list of arrays
+
+    inds : list, or list of lists
+        A list of indices representing rows with missing data. If a list of
+        numpy arrays is passed, a list of lists will be returned.
+
+    """
+
+    x = format_data(x)
+
+    inds = [[idx for idx,row in enumerate(arr) if any(np.isnan(row))] for arr in x]
+
+    if len(inds) > 1:
+        return inds
+    else:
+        return inds[0]
