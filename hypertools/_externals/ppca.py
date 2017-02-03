4f1943d373f60e1f6f8de9d624e45dc55a6487f9
andrewheusser <andrew.heusser@gmail.com>
2017-02-03 13:12:05 -0500

    used futurize to add support for python3.6

---
+++ hypertools/_externals/ppca.py
@@ -0,0 +1,146 @@
+from __future__ import division
+from __future__ import print_function
+
+from builtins import object
+import os
+
+import numpy as np
+from scipy.linalg import orth
+
+
+class PPCA(object):
+
+    def __init__(self):
+
+        self.raw = None
+        self.data = None
+        self.C = None
+        self.means = None
+        self.stds = None
+
+    def _standardize(self, X):
+
+        if self.means is None or self.stds is None:
+            raise RuntimeError("Fit model first")
+
+        return (X - self.means) / self.stds
+
+    def fit(self, data, d=None, tol=1e-4, min_obs=10, verbose=False):
+
+        self.raw = data
+        self.raw[np.isinf(self.raw)] = np.max(self.raw[np.isfinite(self.raw)])
+
+        valid_series = np.sum(~np.isnan(self.raw), axis=0) >= min_obs
+
+        data = self.raw[:, valid_series].copy()
+        N = data.shape[0]
+        D = data.shape[1]
+
+        self.means = np.nanmean(data, axis=0)
+        self.stds = np.nanstd(data, axis=0)
+
+        data = self._standardize(data)
+        observed = ~np.isnan(data)
+        missing = np.sum(~observed)
+        data[~observed] = 0
+
+        # initial
+
+        if d is None:
+            d = data.shape[1]
+
+        if self.C is None:
+            C = np.random.randn(D, d)
+        else:
+            C = self.C
+        CC = np.dot(C.T, C)
+        X = np.dot(np.dot(data, C), np.linalg.inv(CC))
+        recon = np.dot(X, C.T)
+        recon[~observed] = 0
+        ss = np.sum((recon - data)**2)/(N*D - missing)
+
+        v0 = np.inf
+        counter = 0
+
+        while True:
+
+            Sx = np.linalg.inv(np.eye(d) + CC/ss)
+
+            # e-step
+            ss0 = ss
+            if missing > 0:
+                proj = np.dot(X, C.T)
+                data[~observed] = proj[~observed]
+            X = np.dot(np.dot(data, C), Sx) / ss
+
+            # m-step
+            XX = np.dot(X.T, X)
+            C = np.dot(np.dot(data.T, X), np.linalg.pinv(XX + N*Sx))
+            CC = np.dot(C.T, C)
+            recon = np.dot(X, C.T)
+            recon[~observed] = 0
+            ss = (np.sum((recon-data)**2) + N*np.sum(CC*Sx) + missing*ss0)/(N*D)
+
+            # calc diff for convergence
+            det = np.log(np.linalg.det(Sx))
+            if np.isinf(det):
+                det = abs(np.linalg.slogdet(Sx)[1])
+            v1 = N*(D*np.log(ss) + np.trace(Sx) - det) \
+                + np.trace(XX) - missing*np.log(ss0)
+            diff = abs(v1/v0 - 1)
+            if verbose:
+                print(diff)
+            if (diff < tol) and (counter > 5):
+                break
+
+            counter += 1
+            v0 = v1
+
+
+        C = orth(C)
+        vals, vecs = np.linalg.eig(np.cov(np.dot(data, C).T))
+        order = np.flipud(np.argsort(vals))
+        vecs = vecs[:, order]
+        vals = vals[order]
+
+        C = np.dot(C, vecs)
+
+        # attach objects to class
+        self.C = C
+        self.data = data
+        self.eig_vals = vals
+        self._calc_var()
+
+        import IPython
+        IPython.embed()
+        assert False
+
+    def transform(self, data=None):
+
+        if self.C is None:
+            raise RuntimeError('Fit the data model first.')
+        if data is None:
+            return np.dot(self.data, self.C)
+        return np.dot(data, self.C)
+
+    def _calc_var(self):
+
+        if self.data is None:
+            raise RuntimeError('Fit the data model first.')
+
+        data = self.data.T
+
+        # variance calc
+        var = np.nanvar(data, axis=1)
+        total_var = var.sum()
+        self.var_exp = self.eig_vals.cumsum() / total_var
+
+    def save(self, fpath):
+
+        np.save(fpath, self.C)
+
+    def load(self, fpath):
+
+        assert os.path.isfile(fpath)
+
+        self.C = np.load(fpath)
