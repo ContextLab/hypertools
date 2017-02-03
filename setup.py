2c68ce9675cc5b6e5d0ec96273400fbd95129ef3
andrewheusser <andrew.heusser@gmail.com>
2017-02-03 13:25:49 -0500

    advancing version number for pip

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ LICENSE = 'MIT'
 
 setup(
     name='hypertools',
-    version='0.1.2',
+    version='0.1.3',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Contextual Dynamics Lab',
