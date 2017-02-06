1c6db7c82067e7ee196b0203a3fb948665895605
andrewheusser <andrew.heusser@gmail.com>
2017-02-06 08:45:03 -0500

    advanced version for pip release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ LICENSE = 'MIT'
 
 setup(
     name='hypertools',
-    version='0.1.4',
+    version='0.1.5',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Contextual Dynamics Lab',
