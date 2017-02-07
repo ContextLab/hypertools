4021e59f743d11d46f92caddb8edec59ffd0c26d
andrewheusser <andrew.heusser@gmail.com>
2017-02-07 17:13:12 -0500

    remove <2.0 matplotlib req

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
    'scikit-learn>=0.18.1',
    'pandas>=0.18.0',
    'seaborn>=0.7.1',
-   'matplotlib>=1.5.1,<2.0',
+   'matplotlib>=1.5.1',
    'scipy>=0.17.1',
    'numpy>=1.10.4',
    'future'
