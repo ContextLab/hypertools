b056d5a66b20b1be790a839a158ffe839bb5566c
andrewheusser <andrew.heusser@gmail.com>
2017-02-23 16:52:42 -0500

    incremented version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ LICENSE = 'MIT'
 
 setup(
     name='hypertools',
-    version='0.1.5',
+    version='0.1.6',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Contextual Dynamics Lab',
