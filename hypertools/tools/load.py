6ab364a09e0af7966d8b8e6bd5a1cbb4c516db4a
andrewheusser <andrew.heusser@gmail.com>
2017-04-15 07:40:16 -0400

    fixed issue related to encoding

--- hypertools/tools/load.py
+++ hypertools/tools/load.py
@@ -1,6 +1,7 @@
 import requests
 import pickle
 import pandas as pd
+import sys
 
 def load(dataset):
 	"""
@@ -17,15 +18,21 @@ def load(dataset):
         Example data
 
     """
+	if sys.version_info[0]==3:
+		pickle_options = {
+			'encoding' : 'latin1'
+		}
+	else:
+		pickle_options = {}
 
 	if dataset is 'weights':
 		fileid = '0B7Ycm4aSYdPPREJrZ2stdHBFdjg'
 		url = 'https://docs.google.com/uc?export=download&id=' + fileid
-		data = pickle.loads(requests.get(url, stream=True).content)
+		data = pickle.loads(requests.get(url, stream=True).content, **pickle_options)
 	elif dataset is 'spiral':
 		fileid = '0B7Ycm4aSYdPPQS0xN3FmQ1FZSzg'
 		url = 'https://docs.google.com/uc?export=download&id=' + fileid
-		data = pickle.loads(requests.get(url, stream=True).content)
+		data = pickle.loads(requests.get(url, stream=True).content, **pickle_options)
 	elif dataset is 'mushrooms':
 		fileid = '0B7Ycm4aSYdPPY3J0U2tRNFB4T3c'
 		url = 'https://docs.google.com/uc?export=download&id=' + fileid
