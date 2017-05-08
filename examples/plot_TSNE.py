dce3b66b54fac2040e826a5a465ff58cf1295e7f
andrewheusser <andrew.heusser@gmail.com>
2017-05-08 17:41:17 -0400

    adding addition decomposition/manifold learning models, WIP

---
+++ examples/plot_TSNE.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+=============================
+Visualizing the digits dataset using t-SNE
+=============================
+
+This example loads in some data from the scikit-learn digits dataset and plots
+it using t-SNE. Other supported models include PCA, FastICA, MDS, Isomap,
+SpectralEmbedding and LocallyLinearEmbedding
+
+"""
+
+# Code source: Andrew Heusser
+# License: MIT
+
+from sklearn import datasets
+import hypertools as hyp
+
+digits = datasets.load_digits(n_class=6)
+data = digits.data
+group = digits.target
+
+hyp.plot(data, 'o', model='TSNE', group=group)
