de5fabaf9eefe0efbd6fca88c194330eb1452081
andrewheusser <andrew.heusser@gmail.com>
2017-05-11 11:51:29 -0400

    removed extra class in init

--- hypertools/__init__.py
+++ hypertools/__init__.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
 
-from builtins import object
 from .plot.plot import plot
 from .tools import *
-
-class hypertools(object):
-    '''Hypertools module'''
-
-    def __init__(self, plot=plot, tools=tools):
-        self.plot = plot
-        self.tools = tools
