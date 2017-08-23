5df0133e97ae0d5c96f51ae4b321f6a5fb5c5116
Chase Williams <bornofawesomeness@gmail.com>
2017-08-23 18:21:37 -0400

    fixed noshow notebook error

--- hypertools/plot/plot.py
+++ hypertools/plot/plot.py
@@ -276,8 +276,9 @@ def plot(x, fmt=None, marker=None, markers=None, linestyle=None,
     x = scale(x)
 
     # handle palette with seaborn
-    sns.set_palette(palette=palette, n_colors=len(x))
-    sns.set_style(style='whitegrid')
+    if not show:
+        sns.set_palette(palette=palette, n_colors=len(x))
+        sns.set_style(style='whitegrid')
 
     # turn kwargs into a list
     kwargs_list = parse_kwargs(x, mpl_kwargs)
@@ -304,7 +305,8 @@ def plot(x, fmt=None, marker=None, markers=None, linestyle=None,
                             frame_rate=frame_rate,
                             elev=elev,
                             azim=azim,
-                            explore=explore)
+                            explore=explore,
+                            show=False)
 
     # tighten layout
     plt.tight_layout()
