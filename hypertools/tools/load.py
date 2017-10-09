16a4571e57a664e6d4a84349bea8bee37656fc13
andrewheusser <andrew.heusser@gmail.com>
2017-10-09 15:19:56 -0400

    fixed doc string for load function

--- hypertools/tools/load.py
+++ hypertools/tools/load.py
@@ -22,13 +22,33 @@ def load(dataset, reduce=None, ndims=None, align=None, normalize=None):
         highlight the `procrustes` function.  `mushrooms` is an example dataset
         comprised of features (columns) of a collection of mushroomm samples (rows).
 
-    normalize : str or None
+    normalize : str or False or None
+        If set to 'across', the columns of the input data will be z-scored
+        across lists (default). That is, the z-scores will be computed with
+        with repect to column n across all arrays passed in the list. If set
+        to 'within', the columns will be z-scored within each list that is
+        passed. If set to 'row', each row of the input data will be z-scored.
+        If set to False, the input data will be returned with no z-scoring.
 
-    reduce : str or dict or None
-        If not None, reduce data to ndims dimensions
+    reduce : str or dict
+        Decomposition/manifold learning model to use.  Models supported: PCA,
+        IncrementalPCA, SparsePCA, MiniBatchSparsePCA, KernelPCA, FastICA,
+        FactorAnalysis, TruncatedSVD, DictionaryLearning, MiniBatchDictionaryLearning,
+        TSNE, Isomap, SpectralEmbedding, LocallyLinearEmbedding, and MDS. Can be
+        passed as a string, but for finer control of the model parameters, pass
+        as a dictionary, e.g. reduce={'model' : 'PCA', 'params' : {'whiten' : True}}.
+        See scikit-learn specific model docs for details on parameters supported
+        for each model.
 
-    align : str or None
-        If True, run data through alignment algorithm in tools.alignment
+    ndims : int
+        Number of dimensions to reduce
+
+    align : str or dict
+        If str, either 'hyper' or 'SRM'.  If 'hyper', alignment algorithm will be
+        hyperalignment. If 'SRM', alignment algorithm will be shared response
+        model.  You can also pass a dictionary for finer control, where the 'model'
+        key is a string that specifies the model and the params key is a dictionary
+        of parameter values (default : 'hyper').
 
     Returns
     ----------
