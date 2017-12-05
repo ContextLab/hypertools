16ab0b0340add865582da38a2e0f4ccd9c4739cf
alok kumar <alok@karza.in>
2017-12-06 01:26:03 +0530

    Issue#146 and Issue#173 Feature Enhancement: GaussianMixture and BayesianGaussianMixture Clustering algoritms added along with documentation and testcases

--- hypertools/tools/cluster.py
+++ hypertools/tools/cluster.py
@@ -1,27 +1,81 @@
 #!/usr/bin/env python
 import warnings
 from sklearn.cluster import KMeans, MiniBatchKMeans, AgglomerativeClustering, Birch, FeatureAgglomeration, SpectralClustering
-from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
 
 import numpy as np
 from .._shared.helpers import *
 
 
 class Cluster(object):
+    """ 
+        Cluster Class holds generic attributes
+        Parameters
+        ---------- 
+        x : A Numpy array, Pandas Dataframe or list of arrays/dfs
+        The data to be clustered.  You can pass a single array/df or a list.
+        If a list is passed, the arrays will be stacked and the clustering
+        will be performed across all lists (i.e. not within each list).
+
+        model: Model to use to discover clusters.  Support algorithms are: KMeans,
+        MiniBatchKMeans, AgglomerativeClustering, Birch, FeatureAgglomeration,
+        SpectralClustering, GaussianMixture, BayesianGaussianMixture. See scikit-learn specific model docs 
+        for details on parameters supported for each model.
+
+        Attributes
+        ----------
+        model_params: dict  
+            This parameter is especially useful for a finer control of the model parameters, e.g. one can 
+            set the maximum number of iteration for KMeans by setting the value of model_params as {'max_iter' : 100}. See
+            scikit-learn specific model docs for details on parameters supported for each model.
+
+    """
     def __init__(self,x,model):
         self.x=x
         self.model=model
     def set_num_clusters(self,n_clusters):
+        """
+        Sets the number of expected clusters
+        Parameters
+        ----------    
+        n_clusters : int
+            Number of clusters to discover
+
+        """
         self.model_params = {
         'n_clusters' : n_clusters
         }
     def set_custom_params(self,custom_params):
+        """
+        Sets the Parameters of the underlying Scikit-learn's Clustering algorithm. See
+        scikit-learn specific model docs for details on parameters supported for each model.
+        Parameters
+        ----------    
+        custom_params : dict
+            Parameters to be passed to the underlying Scikit-learn's Clustering algorithm 
+
+        """
         self.model_params = custom_params
     def initialize_model(self):
+        """
+        Initializes the underlying Scikit-learn's Clustering algorithm with set model parameters
+        """
         self.model = self.model(**self.model_params)
     def fit_model(self):
+        """
+        Computes the Scikit-learn clustering
+        """
         self.model.fit(np.vstack(self.x))
     def get_labels(self):
+        """
+        Returns a list or a list of numpy arrays
+            A list of cluster labels is returned in case of KMeans, MiniBatchKMeans, AgglomerativeClustering, Birch, FeatureAgglomeration, SpectralClustering
+            A list of cluster label probabilities across each cluster is returned in case of GaussianMixture, BayesianGaussianMixture
+        This method should be called only after the underlying Scikit-learn Clustering model is trained using the fit_model() method.
+        Returns
+        ----------
+        A list of cluster labels or A list of cluster label probabilities across each cluster
+        """
         return list(self.model.labels_)
 class KMeansCluster(Cluster):
     def __init__(self,x):
@@ -49,13 +103,15 @@ class GaussianMixtureCluster(Cluster):
         'n_components' : n_clusters
         }
     def get_labels(self):
-        return list(self.model.predict(np.vstack(self.x)))
-
+        return list(self.model.predict_proba(np.vstack(self.x)))
+class BayesianGaussianMixtureCluster(GaussianMixtureCluster):
+    def __init__(self,x):
+        Cluster.__init__(self,x,BayesianGaussianMixture)
 
 @memoize
 def cluster(x, cluster='KMeans', n_clusters=3, ndims=None):
     """
-    Performs clustering analysis and returns a list of cluster labels
+    Performs clustering analysis and returns a list of cluster labels or label probabilities(In the case of Gaussian Mixture models).
 
     Parameters
     ----------
@@ -67,7 +123,7 @@ def cluster(x, cluster='KMeans', n_clusters=3, ndims=None):
     cluster : str or dict
         Model to use to discover clusters.  Support algorithms are: KMeans,
         MiniBatchKMeans, AgglomerativeClustering, Birch, FeatureAgglomeration,
-        SpectralClustering, GaussianMixture (default: KMeans).Can be passed as a string, but for
+        SpectralClustering, GaussianMixture, BayesianGaussianMixture (default: KMeans).Can be passed as a string, but for
         finer control of the model parameters, pass as a dictionary, e.g.
         reduce={'model' : 'KMeans', 'params' : {'max_iter' : 100}}. See
         scikit-learn specific model docs for details on parameters supported for
@@ -82,8 +138,9 @@ def cluster(x, cluster='KMeans', n_clusters=3, ndims=None):
 
     Returns
     ----------
-    cluster_labels : list
-        An list of cluster labels
+    cluster_labels : list or list of numpy arrays
+        A list of cluster labels in case of KMeans, MiniBatchKMeans, AgglomerativeClustering, Birch, FeatureAgglomeration, SpectralClustering
+        A list of cluster label probabilities across each cluster in case of GaussianMixture, BayesianGaussianMixture
 
     """
 
@@ -105,7 +162,8 @@ def cluster(x, cluster='KMeans', n_clusters=3, ndims=None):
             'FeatureAgglomeration' : FeatureAgglomerationCluster,
             'Birch' : BirchCluster,
             'SpectralClustering' : SpectralClusteringCluster,
-            'GaussianMixture'   : GaussianMixtureCluster
+            'GaussianMixture'   : GaussianMixtureCluster,
+            'BayesianGaussianMixture' : BayesianGaussianMixtureCluster
         }
 
 
--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -10,10 +10,21 @@ from hypertools.tools.cluster import cluster
 cluster1 = np.random.multivariate_normal(np.zeros(3), np.eye(3), size=100)
 cluster2 = np.random.multivariate_normal(np.zeros(3)+100, np.eye(3), size=100)
 data = np.vstack([cluster1,cluster2])
-labels = cluster(data,n_clusters=2)
+labels_kmeans = cluster(data,n_clusters=2)
+labels_kmeans_custom=cluster(data,cluster={'model':'KMeans','params':{'n_clusters':3}})
+labels_gaussian_prob=cluster(data, cluster='GaussianMixture', n_clusters=2)
+labels_gaussian_prob_custom=cluster(data, cluster={'model':'GaussianMixture','params':{'n_components':3}})
+labels_bayesian_gaussian_prob=cluster(data, cluster='BayesianGaussianMixture', n_clusters=2)
+labels_bayesian_gaussian_prob_custom=cluster(data, cluster={'model':'BayesianGaussianMixture','params':{'n_components':3}})
 
 def test_cluster_n_clusters():
-    assert len(set(labels))==2
+    assert (len(set(labels_kmeans))==2) and (len(set(labels_kmeans_custom))==3) and (len(labels_gaussian_prob[0])==2) and \
+    	(len(labels_gaussian_prob_custom[0])==3) and (len(labels_bayesian_gaussian_prob[0])==2) and (len(labels_bayesian_gaussian_prob_custom[0])==3)
 
 def test_cluster_returns_list():
-    assert type(labels) is list
+    assert (type(labels_kmeans) is list) and (type(labels_kmeans_custom) is list) and (type(labels_gaussian_prob) is list) and \
+    	(type(labels_gaussian_prob_custom) is list) and (type(labels_bayesian_gaussian_prob) is list) and (type(labels_bayesian_gaussian_prob_custom) is list)
+
+def test_cluster_gaussian_returns_list_of_probs():
+	assert (type(labels_gaussian_prob[0]) is np.ndarray) and (type(labels_gaussian_prob_custom[0]) is np.ndarray) and \
+		(type(labels_bayesian_gaussian_prob[0]) is np.ndarray) and (type(labels_bayesian_gaussian_prob_custom[0]) is np.ndarray)
