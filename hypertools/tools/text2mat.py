df89e0abf24293662487be4edb7c22b9254784ed
andrewheusser <andrew.heusser@gmail.com>
2018-01-19 14:40:45 -0500

    saving progress on text2mat

--- hypertools/tools/text2mat.py
+++ hypertools/tools/text2mat.py
@@ -1,6 +1,8 @@
 import numpy as np
+import inspect
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation, NMF
+from sklearn.utils.validation import check_is_fitted
 from sklearn.pipeline import Pipeline
 from .._shared.helpers import format_data
 
@@ -51,7 +53,7 @@ def text2mat(data, vectorizer='count', vectorizer_params=None, text_model='LDA',
     """
 
     # subfunction to loop over arrays
-    def transform_list(x, model, text_model, fit_model):
+    def transform_list(x, model, fit_model):
         split = np.cumsum([len(xi) for xi in x])[:-1]
         if text_model is None:
             if fit_model:
@@ -69,14 +71,44 @@ def text2mat(data, vectorizer='count', vectorizer_params=None, text_model='LDA',
         else:
             return [x_r[0]]
 
+    # check the type of the  param
+    def check_type(x):
+        if type(x) is str:
+            return 'str'
+        elif type(x) is dict:
+            return 'dict'
+        elif inspect.isclass(x):
+            return 'class'
+        elif x is None:
+            return 'None'
+        else:
+            try:
+                if inspect.isclass(type(x)):
+                    return 'class_instance'
+            except:
+                raise TypeError('Parameter must of type string, dict, class, or'
+                                ' class instance.')
+
+    # check the type of the vectorizer model
+    vtype = check_type(vectorizer)
+
+    # check the type of the vectorizer model
+    ttype = check_type(text_model)
+
     # vector models
     vectorizer_models = {
         'count' : CountVectorizer,
         'tfidf' : TfidfVectorizer
     }
 
-    # support user defined vectorizers
-    if hasattr(vectorizer, '__class__'):
+    # text models
+    text_models = {
+        'LDA' : LatentDirichletAllocation,
+        'NMF' : NMF
+    }
+
+    # support user defined vectorizer model
+    if vtype in ('class', 'class_instance'):
         if hasattr(vectorizer, 'fit_transform'):
             vectorizer_models.update({'user_model' : vectorizer})
             vectorizer = 'user_model'
@@ -86,17 +118,11 @@ def text2mat(data, vectorizer='count', vectorizer_params=None, text_model='LDA',
                                'for more details: '
                                'http://scikit-learn.org/stable/data_transforms.html')
 
-    # text models
-    text_models = {
-        'LDA' : LatentDirichletAllocation,
-        'NMF' : NMF
-    }
-
     # support user defined text model
-    if hasattr(text_model, '__class__'):
+    if ttype in ('class', 'class_instance'):
         if hasattr(text_model, 'fit_transform'):
             text_models.update({'user_model' : text_model})
-            text = 'user_model'
+            text_model = 'user_model'
         else:
             raise RuntimeError('Error: Text model must have fit_transform '
                                'method following the scikit-learn API. See here '
@@ -104,39 +130,42 @@ def text2mat(data, vectorizer='count', vectorizer_params=None, text_model='LDA',
                                'http://scikit-learn.org/stable/data_transforms.html')
 
 
-    # vector params
+    # init model params
     if vectorizer_params is None:
         vectorizer_params = {}
-
-    # text params
     if text_params is None:
         text_params = {}
 
     # update text dict with n_topics
-    text_params.update({'n_components' : n_topics})
+    text_params.update({'n_components' : n_components})
 
-    # if it has a __name__, it is a class (not a class instance)
-    if hasattr(vectorizer_models[vectorizer], '__name__'):
-        model = vectorizer_models[vectorizer](**vectorizer_params)
-    else:
-        # otherwise, its a class instance so don't iniatilize it
-        model = vectorizer_models[vectorizer]
 
-    # if no text model, just return vectorized text, else, return text fit to model
-    if text_model is not None:
+    if vectorizer:
+        # intialize vectorizer model
+        if vtype in ('str', 'dict', 'class'):
+            vmodel = vectorizer_models[vectorizer](**vectorizer_params)
+        elif vtype is 'class_instance':
+            # otherwise, its a class instance so don't iniatilize it
+            vmodel = vectorizer_models[vectorizer]
 
-        # if it has a __name__, it is a class (not a class instance)
-        if hasattr(text_models[text_model], '__name__'):
+    if text_model:
+        # initialize text model
+        if ttype in ('str', 'dict', 'class'):
             tmodel = text_models[text_model](**text_params)
-        else:
+        elif ttype is 'class_instance':
             # otherwise, its a class instance so don't iniatilize it
             tmodel = text_models[text_model]
 
-        # create a vector->text pipeline
-        model = Pipeline([(vectorizer, model),
+    # if both vectorizer and text model, put them in a pipeline
+    if vectorizer and text_model:
+        model = Pipeline([(vectorizer, vmodel),
                           (text_model, tmodel)])
+    elif vectorizer:
+        model = vmodel
+    else:
+        model = tmodel
 
     # format data into list of arrays
     x = format_data(data)
 
-    return transform_list(x, model, text_model, fit_model)
+    return transform_list(x, model, fit_model)
