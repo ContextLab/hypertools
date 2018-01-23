fe82126a2dc2ea028b45650090f05a808b94b5f4
andrewheusser <andrew.heusser@gmail.com>
2018-01-23 12:10:44 -0500

    added tests for geo text fields

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -38,3 +38,12 @@ def test_geo_transform():
 
 def test_geo_transform_dims():
     assert geo.transform(data)[0].shape[1]==3
+
+def test_geo_text_data():
+    data = [['i like cats alot', 'cats r pretty cool', 'cats are better than dogs'],
+            ['dogs rule the haus', 'dogs are my jam', 'dogs are a mans best friend']]
+    geo = plot(data, show=False)
+    assert isinstance(geo, DataGeometry)
+    assert geo.transform(data)[0].shape[1]==3
+    assert geo.text['model'] is 'LatentDirichletAllocation'
+    assert geo.text['params']=={'n_components' : 20}
