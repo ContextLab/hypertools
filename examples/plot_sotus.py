cc33adbb419052428a0c417e9d2012ad0d551567
andrewheusser <andrew.heusser@gmail.com>
2018-02-19 14:20:42 -0500

    added sotus example

---
+++ examples/plot_sotus.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+=============================
+Plotting State of the Union Addresses from 1989-2017
+=============================
+
+To plot text, simply pass the text data to the plot function.  Here, we will
+plot each SOTU address by first fitting the text data to a topic model, and then
+reducing the dimensionality of the topic vectors to visualize them. By default,
+hypertools transforms the text data using a model fit to a set of selected
+wikipedia pages.
+
+"""
+
+# Code source: Andrew Heusser
+# License: MIT
+
+# load hypertools
+import hypertools as hyp
+
+# load the data
+data, hue, labels = hyp.load('sotus')
+
+# plot it
+hyp.plot(data, 'o', hue=labels, labels=labels)
