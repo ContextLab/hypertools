f2277eab18884cb15847047cc70f57a8a7008fe1
Jeremy Manning <jeremy.manning@gmail.com>
2020-10-03 23:03:25 -0400

    added skeleton class for plot

--- dev/plot/plot.py
+++ dev/plot/plot.py
@@ -1,1 +1,27 @@
-#placeholder file
+class Plot(object):
+    def __init__(self, plotter, **args, **kwargs):
+        self.plotter = plotter
+        self.args = args
+        self.kwargs = kwargs
+
+    def draw(self, data):
+        return self.plotter(data, **self.args, **self.kwargs)
+
+    def save(self, **args, **kwargs):
+        pass
+
+    def get_args(self):
+        return self.args
+
+    def set_args(self, **args):
+        self.args = args
+
+    def get_kwargs(self):
+        return self.kwargs
+
+    def set_kwargs(self, **kwargs):
+        self.kwargs = kwargs
+
+    def update_kwargs(self, **kwargs):
+        for key, val in kwargs:
+            self.kwargs[key] = val
