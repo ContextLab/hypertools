9cc0deb0fe89c9a7d9e5b4cf242ee922d75165be
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-10-08 22:57:29 -0400

    docstrings and notes for switching functions

--- hypertools/plot/backend.py
+++ hypertools/plot/backend.py
@@ -7,6 +7,7 @@ from os import getenv
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# TODO: if person sets backend later after import, maybe should unset BACKEND_WARNING?
 
 HYPERTOOLS_BACKEND = None
 IS_NOTEBOOK = False
@@ -135,27 +136,98 @@ def set_backend():
 
 
 def _switch_notebook_backend(backend):
-    # have to import this here since ipykernel is only guaranteed to
-    # be installed if running in notebook
+    """
+    Handles switching the matplotlib backend when running in a Jupyter
+    notebook
+
+    Parameters
+    ----------
+    backend : str
+        the interactive matplotlib backend to switch to
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    `flush_figures` is a post-cell execution callback that `plt.show()`s
+    & `plt.close()`s all figures created in a cell so that `plt.plot()`
+    calls in later cells create new figures. There's a weird circular
+    matplotlib/IPython interaction where:
+      - matplotlib.pyplot uses IPython.core.pylabtools to register
+        `flush_figures` when it's imported into an IPython environment
+      - The `%matplotlib inline` magic command also registers a
+        `flush_figures` call each it's run, whether or not one has
+        been registered already
+      - IPython runs `%matplotlib inline` if it detects matplotlib.pyplot
+        has been imported and no backend is set in the same cell.
+    Thus, depending on import order, whether imports happen across
+    multiple cells, and whether/when/how many times the backend has been
+    switched, there may be any number of `flush_figures` callbacks
+    registed, and switching to the interactive notebook backend
+    (`plt.switch_backend('nbAgg')`/`%matplotlib notebook`) unregisters
+    one `flush_figures` callback, but leaves the other(s). If you try to
+    plot an interactive figure with `flush_figures` registered, the
+    figure is closed as soon as the cell finishes executing and the
+    matplotlib event loop throws an error. So we need to ensure all
+    `flush_figures` instances are unregistered before plotting.
+    """
+    # ipykernel is only guaranteed to be installed if running in notebook
     from ipykernel.pylab.backend_inline import flush_figures
+
     IPYTHON_INSTANCE.run_line_magic('matplotlib', backend)
-    # see (1) below re: unregistering `flush_figures` callback
     while flush_figures in IPYTHON_INSTANCE.events.callbacks['post_execute']:
         IPYTHON_INSTANCE.events.unregister('post_execute', flush_figures)
 
 
 def _reset_notebook_backend(backend):
-    # see (2) below re: deferring resetting the backend in notebooks
-    def _callback():
-        _callback.ipython_instance.run_line_magic('matplotlib', _callback.backend)
-        _callback.ipython_instance.events.unregister('pre_run_cell', _callback)
+    """
+    Handles resetting the matplotlib backend after displaying an
+    animated/interactive plot in a Jupyter notebook by registering a
+    "one-shot" self-destructing pre-cell-run callback to the next cell
+
+    Parameters
+    ----------
+    backend : str
+        the matplotlib backend to switch back to
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Changing the matplotlib backend in a Jupyter notebook immediately
+    closes any open figures (meaning animation and interactivity stop),
+    so we can't do it as part of the function call or register it as a
+    post-execution hook for the current cell. We get around this by
+    register a callback function that runs *before* the *next* cell is
+    run, resets the backend, and unregisters itself. This way, the
+    animation runs and the plot is interactive until the user runs the
+    next cell, but the backend is reset before any code is executed. And
+    because the callback "self-destructs," we won't force later figures
+    to close unnecessarily ("changing" the backend to the current backend
+    still does this) and doing this multiple times won't pollute the
+    callback list, which can slow down cell execution.
+
+    We also have to define and register the callback inside a wrapper
+    function, since we need to reference the backend we're switching to
+    using the same syntax as `matplotlib.pyplot.switch_backend`. IPython
+    callbacks have to have the same signature as the prototype for the
+    event they're registered to, and the pre_run_cell prototype takes no
+    arguments. And since the callback needs to reference the correct
+    function object in order to unregister itself, we can't just wrap it
+    in `functools.partial`.
+    """
+    def _deferred_reset_cb():
+        IPYTHON_INSTANCE.run_line_magic('matplotlib', backend)
+        IPYTHON_INSTANCE.events.unregister('pre_run_cell', _deferred_reset_cb)
 
     if backend == 'module://ipykernel.pylab.backend_inline':
         backend = 'inline'
 
-    _callback.backend = backend
-    _callback.ipython_instance = IPYTHON_INSTANCE
-    IPYTHON_INSTANCE.events.register('pre_run_cell', _callback)
+    IPYTHON_INSTANCE.events.register('pre_run_cell', _deferred_reset_cb)
 
 
 def _get_plot_kwargs(plot_func, *func_args, **func_kwargs):
@@ -170,7 +242,8 @@ def manage_backend(plot_func):
     Decorator for hypertools.plot that prevents unexpected changes to
     matplotlib rcParams (https://github.com/ContextLab/hypertools/issues/243)
     and handles temporarily changing the matplotlib backend for
-    interactive and animated plots.
+    interactive and animated plots, as necessary.
+
     Parameters
     ----------
     plot_func
@@ -214,29 +287,3 @@ def manage_backend(plot_func):
                 mpl.rcParams.update(**curr_rcParams)
 
     return plot_wrapper
-
-# (1)
-# `flush_figures` is a post-cell execution callback that basically runs
-# `plt.show(); plt.close('all')`. There's a weird matplotlib/IPython
-# interaction bug where:
-#   - matplotlib.pyplot uses IPython.core.pylabtools to register
-#     `flush_figures` when imported into an ipython env
-#   - The `%matplotlib inline` magic command also registers
-#     `flush_figures` the first time it's run
-#   - IPython runs `%matplotlib inline` if it detects matplotlib.pyplot
-#     has been imported and no backend is set in the same cell
-#
-# Switching to the interactive notebook backend (plt.switch_backend('nbAgg')
-# or `%matplotlib notebook`) unregisters *one* `flush_figures` callback, but
-# leaves the other(s), so the interactive figure is closed as soon as it's
-# rendered and the event loop throws an error.
-
-
-# (2)
-# if we just created an interactive plot in a notebook and `main_backend`
-# isn't the current backend, then we're reverting to 'inline'. This will
-# kill any currently running animations, so we can't do it as part of the
-# function call OR part of the current cell, otherwise the figure will
-# close immediately. Instead, we can register an IPython callback
-# function that runs *before* execution of the next cell, resets the
-# backend, and unregisters itself all at once.
