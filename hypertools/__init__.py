cb0eccefc78fb8187336f98e7e585fda9e963be4
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-10-19 11:31:48 -0400

    fixed name for import

--- hypertools/__init__.py
+++ hypertools/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from .config import __version__
 from .plot.plot import plot
-from .plot.backend import set_backend
+from .plot.backend import set_interactive_backend
 from .tools.load import load
 from .tools.analyze import analyze
 from .tools.reduce import reduce
