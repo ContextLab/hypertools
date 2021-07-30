4f764991188477aba5d09ebf8e9d5f78e2f8e301
jeremymanning <jeremy.manning@gmail.com>
2021-07-30 09:56:01 -0400

    finished draft of API for applying models

--- dev/core/model.py
+++ dev/core/model.py
@@ -2,9 +2,10 @@
 import datawrangler as dw
 import numpy as np
 import os
-import sklearn, flair, umap
+import sklearn
+import flair
+import umap
 from sklearn.experimental import enable_hist_gradient_boosting, enable_iterative_imputer, enable_halving_search_cv
-
 
 defaults = dw.core.get_default_options('config.ini')
 sklearn_modules = ['calibration', 'cluster', 'compose', 'covariance', 'cross_decompositoin', 'decomposition',
@@ -19,7 +20,67 @@ sklearn_modules.append('umap')
 flair_embeddings = [f'flair.embeddings.{f}' for f in dir(flair.embeddings) if 'embedding' in f.lower()]
 
 
-def apply_model(data, model, *args, mode='fit_transform', return_model=False, **kwargs):
+def has_all_attributes(x, attributes):
+    return all([hasattr(x, a) for a in attributes])
+
+
+def has_any_attributes(x, attributes):
+    return any([hasattr(x, a) for a in attributes])
+
+
+def get_model(x):
+    if type(x) is str:
+        for m in [*sklearn_modules, *flair_embeddings]:
+            try:
+                exec(f'import {m}.{x}', globals())
+                return eval(f'{m}.{x}')
+            except ModuleNotFoundError:
+                continue
+        return None
+    elif callable(x):
+        if hasattr(x, 'fit') and has_any_attributes(x, ['transform', 'fit_transform', 'predict', 'fit_predict',
+                                                        'predict_proba']) or \
+                hasattr(x, 'embed'):
+            return x
+    raise ValueError(f'unknown model: {x}')
+
+
+def get_sklearn_method(x, mode):
+    def helper(m):
+        if hasattr(x, m):
+            return getattr(x, m)
+        elif ('fit' in mode) and has_all_attributes(x, ['fit', m.replace('fit_', '')]):
+            return [getattr(x, 'fit'), getattr(x, m.replace('fit_', ''))]
+        else:
+            raise ValueError(f'mode not supported for {x}: {m}')
+
+    if 'transform' in mode:  # transform or fit_transform
+        if hasattr(model, 'transform'):
+            return helper(mode)
+        elif hasattr(model, 'predict'):
+            return helper(mode.replace('transform', 'predict'))
+        elif hasattr(model, 'predict_proba'):
+            return helper(mode.replace('transform', 'predict_proba'))
+    elif 'predict_proba' in mode:  # predict_proba or fit_predict_proba
+        if hasattr(model, 'predict_proba'):
+            return helper(mode)
+        elif hasattr(model, 'transform'):
+            return helper(mode.replace('predict_proba', 'transform'))
+        elif hasattr(model, 'predict'):
+            return helper(mode.replace('predict_proba', 'predict'))
+    elif 'predict' in mode:  # predict or fit_predict
+        if hasattr(model, 'predict'):
+            return helper(mode)
+        elif hasattr(model, 'transform'):
+            return helper(mode.replace('predict', 'transform'))
+        elif hasattr(model, 'predict_proba'):
+            return helper(mode.replace('transform', 'predict_proba'))
+    return helper(mode)
+
+
+def apply_model(data, model, *args, return_model=False, **kwargs):
+    mode = kwargs.pop('mode', 'fit_transform')
+
     if type(model) is list:
         fitted_models = []
         for m in model:
@@ -31,22 +92,25 @@ def apply_model(data, model, *args, mode='fit_transform', return_model=False, **
             return data
 
     elif type(model) is dict:
-        assert all([hasattr(model, k) for k in ['model', 'args', 'kwargs']]),\
+        assert all([hasattr(model, k) for k in ['model', 'args', 'kwargs']]), \
             ValueError('model must have keys "model", "args", and "kwargs"')
 
         return apply_model(data, model['model'], return_model=return_model,
                            *[model['args'], *args], **dw.core.update_dict(model['kwargs'], kwargs))
 
     else:
-        model = extract_model(model)
+        model = dw.core.apply_defaults(get_model(model))(*args, **kwargs)
+        if dw.zoo.text.is_hugging_face_model(model):
+            return dw.zoo.text.apply_text_model(model, data, *args, mode=mode, return_model=return_model, **kwargs)
+        f = get_sklearn_method(model, mode)
+        if type(f) is list:
+            assert len(f) == 2, ValueError(f'bad mode: {mode}')
+            f[0](data)
+            transformed_data = f[1](data)
+        else:
+            transformed_data = f(data)
 
-    elif callable(model):
-        model = apply_defaults(model)(*args, **kwargs)
-
-        # scikit-learn-like?
-        if dw.zoo.text.is_sklearn_model(model):
-            assert mode in ['fit', 'transform', 'fit_transform']
-
-
-
-
+        if return_model:
+            return transformed_data, model
+        else:
+            return transformed_data
