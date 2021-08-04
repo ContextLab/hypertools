6f6d6082b1cd4773d5b30fc387ce46ab4606a94e
jeremymanning <jeremy.manning@gmail.com>
2021-08-04 18:05:39 -0400

    added group plotting for multiindex dataframes (untested!)  [WIP]

--- hypertools/plot/static.py
+++ hypertools/plot/static.py
@@ -6,6 +6,20 @@ from ..core import get_default_options, eval_dict
 defaults = eval_dict(get_default_options()['plot'])
 
 
+def group_mean(x):
+    @dw.decorate.apply_unstacked
+    def helper(y):
+        return pd.DataFrame(y.mean(axis=0)).T
+
+    means = helper(x)
+    if hasattr(means.index, 'levels'):
+        n_levels = len(means.index.levels)
+        if n_levels > 1:
+            index = pd.MultiIndex.from_frame(means.index.to_frame().iloc[:, :-1])
+            return pd.DataFrame(data=means.values, columns=means.columns, index=index)
+    return means
+
+
 def static_plot(data, **kwargs):
     kwargs = dw.core.update_dict(defaults, kwargs)
 
@@ -16,5 +30,22 @@ def static_plot(data, **kwargs):
         for i, d in enumerate(data):
             opts = {'color': get(color, i), 'ax': ax}
             return static_plot(d, **dw.core.update_dict(kwargs, opts))
+
+    color = get(color, data.shape[0], axis=0)
+    if dw.zoo.is_multiindex_dataframe(data):
+        color_df = pd.DataFrame(color, index=data.index)
+        group_means = group_mean(data)
+        group_colors = group_mean(color_df).values
+
+        scale_properties = ['linewidth', 'markersize', '_alpha']
+        group_kwargs = kwargs.copy()
+        for s in scale_properties:
+            if s[0] == '_':
+                group_kwargs[s] /= defaults['plot']['scale']
+            else:
+                group_kwargs[s] *= defaults['plot']['scale']
+        group_kwargs['color'] = group_colors
+
+        static_plot(group_means, **group_kwargs)
 
     return ax.plot(*data.split(data.shape[1], axis=1), color=color, **kwargs)
