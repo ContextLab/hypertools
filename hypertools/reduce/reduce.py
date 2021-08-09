11c9f8325383a91504572e2e41e68ab344f2b67a
jeremymanning <jeremy.manning@gmail.com>
2021-08-09 10:52:20 -0400

    backing up (debugging reduce module)

--- hypertools/reduce/reduce.py
+++ hypertools/reduce/reduce.py
@@ -10,16 +10,36 @@ from ..align.common import pad
 defaults = get_default_options()
 
 
+def get_n_components(model, **kwargs):
+    if 'n_components' in kwargs.keys():
+        return kwargs['n_components']
+
+    if type(model) is str:
+        if model in ['SparseCoder']:
+            if 'dictionary' in kwargs.keys():
+                return kwargs['dictionary'].shape[1]
+        elif model == 'PPCA':
+            return None
+        else:
+            return defaults[model].copy().pop('n_components', None)
+    elif hasattr(model, '__name__'):
+        return get_n_components(getattr(model, '__name__'), **kwargs)
+    else:
+        return None
+
+
 @dw.decorate.apply_stacked
 def reduce(data, model='IncrementalPCA', **kwargs):
-    n_components = kwargs.pop('n_components', eval(defaults['reduce']['n_components']))
-    kwargs['n_components'] = n_components
+    def returner(data, result, **kwargs):
+        stack_result = dw.zoo.is_multiindex_dataframe(data)
 
-    if data.shape[1] > n_components:
-        return apply_model(data, model, search=['sklearn.decomposition', 'sklearn.manifold', 'sklearn.mixture',
-                                                'umap', 'ppca'],
-                           **dw.core.update_dict(get_default_options()['reduce'], kwargs))
+    n_components = get_n_components(model, **kwargs)
+
+    if (n_components is None) or (data.shape[1] > n_components):
+        return dw.unstack(apply_model(data, model, search=['sklearn.decomposition', 'sklearn.manifold',
+                                                           'sklearn.mixture', 'umap', 'ppca'],
+                                      **dw.core.update_dict(get_default_options()['reduce'], kwargs)))
     elif data.shape[1] == n_components:
-        return data
+        return dw.unstack(data)
     else:
-        return pad(data, c=n_components)
+        return dw.unstack(pad(data, c=n_components))
--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -1,8 +1,37 @@
+# noinspection PyPackageRequirements
+import datawrangler as dw
 import numpy as np
 import pandas as pd
 
 import hypertools as hyp
 
+models = ['DictionaryLearning', 'FactorAnalysis', 'FastICA', 'IncrementalPCA', 'KernelPCA', 'LatentDirichletAllocation',
+          'MiniBatchDictionaryLearning', 'MiniBatchSparsePCA', 'NMF', 'PCA', 'SparsePCA', 'SparseCoder', 'TruncatedSVD',
+          'Isomap', 'LocallyLinearEmbedding', 'MDS', 'SpectralEmbedding', 'TSNE', 'UMAP', 'PPCA']
+
+
+normalized_weights = hyp.manip(hyp.load('weights_sample'), 'Normalize')
+
 
 def test_reduce():
-    pass
+    n_components = 10
+    for m in models:
+        if m == 'SparseCoder':
+            dictionary = dw.stack(hyp.reduce(normalized_weights, 'PCA', n_components=n_components)).values
+            next_model = {'model': m, 'args': [], 'kwargs': {'dictionary': dictionary}}
+        else:
+            next_model = {'model': m, 'args': [], 'kwargs': {'n_components': n_components}}
+
+        reduced_weights = hyp.reduce(normalized_weights, model=next_model)
+        assert type(reduced_weights) is list
+        assert len(reduced_weights) == len(normalized_weights)
+        assert all([r.shape[0] == w.shape[0] for r, w in zip(reduced_weights, normalized_weights)])
+        assert all([r.shape[1] == n_components for r in reduced_weights])
+
+        x = hyp.reduce(normalized_weights[0], model=next_model)
+        assert type(x) is pd.DataFrame
+        assert x.shape[0] == normalized_weights[0].shape[0]
+        assert x.shape[1] == n_components
+
+
+test_reduce()
