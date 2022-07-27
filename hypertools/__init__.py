942cc89a27255ce7a2b537e7b1b4bfd2e26842e9
Jeremy Manning <jeremy.r.manning@dartmouth.edu>
2022-07-27 09:45:00 -0400

    add missing __init__.py file

---
+++ hypertools/__init__.py
@@ -0,0 +1,8 @@
+from .align import align, pad, trim_and_pad
+from .cluster import cluster
+from .core import get_default_options, get, fullfact, eval_dict
+from .core.shared import RobustDict
+from .io import load, save
+from .manip import manip
+from .plot import plot, write
+from .reduce import reduce
